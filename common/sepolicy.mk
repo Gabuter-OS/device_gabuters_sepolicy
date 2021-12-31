7993c07717e91afd7fe83bdce2078e752e982fa9
mgs28-mh <gsaputra880@gmail.com>
2021-12-31 16:26:28 +0700

    sepolicy: Adapt to gabuters
    Signed-off-by: mgs28-mh <gsaputra880@gmail.com>

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -10,25 +10,25 @@ endif
 endif
 
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/common/public
+    device/gabuters/sepolicy/common/public
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/common/private
+    device/gabuters/sepolicy/common/private
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/common/dynamic \
-    device/lineage/sepolicy/common/system
+    device/gabuters/sepolicy/common/dynamic \
+    device/gabuters/sepolicy/common/system
 else
 BOARD_VENDOR_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/common/dynamic \
-    device/lineage/sepolicy/common/vendor
+    device/gabuters/sepolicy/common/dynamic \
+    device/gabuters/sepolicy/common/vendor
 endif
 
 # Selectively include legacy rules defined by the products
--include device/lineage/sepolicy/legacy-common/sepolicy.mk
+-include device/gabuters/sepolicy/legacy-common/sepolicy.mk
 
 # Include atv rules on atv product
 ifeq ($(PRODUCT_IS_ATV), true)
-include device/lineage/sepolicy/atv/sepolicy.mk
+include device/gabuters/sepolicy/atv/sepolicy.mk
 endif
